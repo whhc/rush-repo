835bc76362512aae8429261d4f1556a9030e3b0f
whhc <gao.guixi@qq.com>
2023-02-02 17:07:16 +0800

    :beer:

---
+++ projects/wakatime/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import './App.css'
+// import { getGistData } from './api/gist.api'
+
+function App() {
+
+  useEffect(() => {
+    // getGistData().then(res => {
+    //   console.log(res)
+    // })
+  })
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App

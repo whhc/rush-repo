3e499e5f894edff2859391beeb089c5c3f9abab7
whhc <gao.guixi@qq.com>
2023-02-03 09:38:05 +0800

    :beer: Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 110,
+  // 使用.gitignore来管理换行
+  endOfLine: 'auto',
+  // 单引号代替双引号
+  singleQuote: true,
+  trailingComma: 'none'
+};
--- projects/wakatime/src/api/gist.api.ts
+++ projects/wakatime/src/api/gist.api.ts
@@ -1,10 +1,9 @@
 // const TOKEN = ``
-const GIST_ID = `50136de400dbee1221ced41a306f5f44`
+const GIST_ID = `50136de400dbee1221ced41a306f5f44`;
 // const GIST_FILENAME = `summaries_2023-01-05.json`
 
 export async function getGistData() {
   const req = await fetch(`https://api.github.com/gists/${GIST_ID}`);
   const gist = await req.json();
-  return gist.files
+  return gist.files;
 }
-

209fe648139950597275ca72c3fe8dca9f57ffd6
whhc <gao.guixi@qq.com>
2023-02-06 14:50:28 +0800

    :package: Change d3 to echarts

--- projects/wakatime/src/views/home/Home.tsx
+++ projects/wakatime/src/views/home/Home.tsx
@@ -1,29 +1,43 @@
-// const useFetchData = () => {
-//   const [data, setData] = useState<GistData>();
-//   const fetchData = async () => {
-//     const response = await fetchGistData();
-//     setData(response);
-//   };
-//
-//   return { data, fetchData };
-// };
+import { useRef } from 'react';
+import * as echarts from 'echarts';
+
+function renderEcharts(dom: HTMLElement) {
+  let myChart = echarts.init(dom);
+  myChart.setOption({
+    title: {
+      text: 'ECharts示例'
+    },
+    tooltip: {},
+    xAxis: {
+      data: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue', 'React']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  });
+}
 
 function Home() {
-  // const { data, fetchData } = useFetchData();
-  //
-  // useEffect(() => {
-  //   console.log(data?.files);
-  // }, [data]);
-  //
+  const chartRef = useRef(null);
   const updateData = () => {
-    console.log(updateData);
+    console.log('updateData');
+    console.log('chartRef: ', chartRef.current);
+    if (chartRef.current) {
+      renderEcharts(chartRef.current);
+    }
   };
 
   return (
-    <div className="w-full flex justify-center">
-      <button className="uppercase btn w-64 rounded-full" onClick={updateData}>
+    <div className="w-full">
+      <button className="uppercase btn w-64 rounded-full block m-2" onClick={updateData}>
         sync
       </button>
+      <div ref={chartRef} className="w-full h-80 bg-blue-50"></div>
     </div>
   );
 }
